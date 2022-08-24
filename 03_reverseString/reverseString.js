7d0b99f4f1593380916b969218e3b072f115d9f6
solomonjamesa <myworkemail998800@gmail.com>
2022-08-24 15:03:51 -0500

    Create reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let arrayReverse = stringArray.reverse();
+    let reverseString = arrayReverse.join();
+    reverseString = reverseString.replace(/,/g,'');
+    return reverseString;
+ 
 };
 
 // Do not edit below this line
