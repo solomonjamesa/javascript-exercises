e6ae7ae22dc04694f3940e4ead0fb8194626edd9
solomonjamesa <myworkemail998800@gmail.com>
2022-08-24 10:06:41 -0500

    Create file with a function that intakes a string and a number and repeats the string the amount of times indicated by the number.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+// Created by solomonJamesa 08/24/2022
+//This function returns a repeated string and tests it against different parameters in the repeatString.spec.js file
+const repeatString = function(string, num) {  //repeats the string 
 
+    let string1 = string; 
+
+    let newString = '';
+
+    if (num < 0){
+        newString = "ERROR";
+    }
+
+    for (i = 0; i < num; i++) {
+       newString += string1;
+    };
+
+
+    
+
+    return newString;
 };
 
 // Do not edit below this line
