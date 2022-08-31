45c5de640265a00727174acdc7cb687da001c2a5
solomonjamesa <myworkemail998800@gmail.com>
2022-08-31 13:58:17 -0500

    Passes 3 of 7 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,39 +1,80 @@
-//const removeFromArray = function(array,num) {
-    //let newArray = array;
-    //let removeFromArray = newArray.splice(num-1,1); 
-    //removeFromArray = newArray;
-    //return removeFromArray;
 
 const removeFromArray = function() {
-   // let newArray = arguments[0];
-   // let removeFromArray = newArray.splice(arguments[1]-1,1); 
-   // removeFromArray = newArray;
-   // return removeFromArray;
+  let argArray = Array.from(arguments);
+  let testArray = argArray[0];
 
-  let numberOfParameters = arguments.length;
-    //if (numberOfParameters == 0){
-        //return removeFromArray = "Error 0 : No arguments detected";
+  argArray.reverse();
+  argArray.pop();
+  argArray.reverse();
 
-    //} else if (numberOfParameters == 1){
-        //return removeFromArray = "Error 1 : Not enough arguments detected";
-
-    //} else if (numberOfParameters == 2){
-    if (numberOfParameters == 2){
-        let newArray = arguments[0];
-        let removeFromArray = newArray.splice(arguments[1]-1,1); 
-        removeFromArray = newArray;
-        return removeFromArray;
-
-    } else if (numberOfParameters ===3){
-        let newArray = arguments[0];
-        let removeFromArray = newArray.splice(arguments[1]-1,1); 
-        removeFromArray = newArray.splice(arguments[2]-1,1); 
-        removeFromArray = newArray;
-        return removeFromArray;
-
-    } else{
-        let removeFromArray = "Logic Error"
+  let ifErrorArray = 1;
+  let isItemIncluded = function(item){
+    if (testArray.includes(item)){
+      return testArray.includes(item);    
+    } else {
+      ifErrorArray = 0;
+      return ifErrorArray;
     }
+  }
+  filteredArray = argArray.filter(isItemIncluded);
+
+  while (ifErrorArray == 0){
+    let removeFromArray = testArray;
+    return removeFromArray;
+  };
+
+ 
+  let i = 0;
+  while (ifErrorArray == 1 && i != argArray.length){
+    if (argArray.length == 1){
+
+        let index = testArray.indexOf(argArray[0]);
+        let removedItem = testArray.splice(index,1); 
+        let finalResultArray =function(item){
+            if (item != removedItem){
+                return true
+            };  
+            return newFilteredArray;
+          };
+        let newFilteredArray = testArray.filter(finalResultArray);
+        let removeFromArray = newFilteredArray;
+
+        return removeFromArray;
+
+      } else if (argArray.length == 2){
+
+        for(i=0;i<argArray.length;){
+        
+          let index = testArray.indexOf(argArray[i]);
+
+          let removedItem = testArray.splice(index,1); 
+
+          let finalResultArray =function(item){
+              if (item != removedItem){
+                  return true
+              };
+              
+              };
+          let newFilteredArray=[];    
+          newFilteredArray = testArray.filter(finalResultArray); 
+          i++;
+          if (i != argArray.length){
+            continue;
+          }else {
+            let removeFromArray = newFilteredArray;
+            return removeFromArray;
+          };
+
+        };  
+
+      } else {
+        let removeFromArray = "Logic error";
+        return removeFromArray;
+      }
+
+  };
+ 
+  
 };
 // Do not edit below this line
 module.exports = removeFromArray;
