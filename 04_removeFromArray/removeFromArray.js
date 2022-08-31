f09be65dc5fef044ec0bc7c2d227c3e544a4395f
solomonjamesa <myworkemail998800@gmail.com>
2022-08-31 15:09:14 -0500

    Pass 4 out of 7 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -18,62 +18,61 @@ const removeFromArray = function() {
   }
   filteredArray = argArray.filter(isItemIncluded);
 
-  while (ifErrorArray == 0){
+  while (ifErrorArray == 0 && filteredArray.length == 0){
     let removeFromArray = testArray;
     return removeFromArray;
   };
+  while(ifErrorArray == 0 && filteredArray.length != 0 || ifErrorArray == 1 && filteredArray.length != 0){
+    ifErrorArray = 1;
+    let i = 0;
+    while (ifErrorArray == 1 && i != filteredArray.length){
+      if (filteredArray.length == 1){
 
- 
-  let i = 0;
-  while (ifErrorArray == 1 && i != argArray.length){
-    if (argArray.length == 1){
-
-        let index = testArray.indexOf(argArray[0]);
-        let removedItem = testArray.splice(index,1); 
-        let finalResultArray =function(item){
-            if (item != removedItem){
-                return true
-            };  
-            return newFilteredArray;
-          };
-        let newFilteredArray = testArray.filter(finalResultArray);
-        let removeFromArray = newFilteredArray;
-
-        return removeFromArray;
-
-      } else if (argArray.length == 2){
-
-        for(i=0;i<argArray.length;){
-        
-          let index = testArray.indexOf(argArray[i]);
-
+          let index = testArray.indexOf(filteredArray[0]);
           let removedItem = testArray.splice(index,1); 
-
           let finalResultArray =function(item){
               if (item != removedItem){
                   return true
-              };
-              
-              };
-          let newFilteredArray=[];    
-          newFilteredArray = testArray.filter(finalResultArray); 
-          i++;
-          if (i != argArray.length){
-            continue;
-          }else {
-            let removeFromArray = newFilteredArray;
-            return removeFromArray;
-          };
+              };  
+              return newFilteredArray;
+            };
+          let newFilteredArray = testArray.filter(finalResultArray);
+          let removeFromArray = newFilteredArray;
+          return removeFromArray;
 
-        };  
+        } else if (filteredArray.length == 2){
 
-      } else {
-        let removeFromArray = "Logic error";
-        return removeFromArray;
-      }
+          for(i=0;i<filteredArray.length;){
+          
+            let index = testArray.indexOf(filteredArray[i]);
 
+            let removedItem = testArray.splice(index,1); 
+
+            let finalResultArray =function(item){
+                if (item != removedItem){
+                    return true
+                };
+                
+                };
+            let newFilteredArray=[];    
+            newFilteredArray = testArray.filter(finalResultArray); 
+            i++;
+            if (i != filteredArray.length){
+              continue;
+            }else {
+              let removeFromArray = newFilteredArray;
+              return removeFromArray;
+            };
+
+          };  
+
+        } else {
+          let removeFromArray = "Logic error";
+          return removeFromArray;
+        }
+
+    };
   };
- 
   
 };
 // Do not edit below this line
