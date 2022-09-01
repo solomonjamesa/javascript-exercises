964435f5dd71db7ec170ef2f1315777e7a1c81ee
solomonjamesa <myworkemail998800@gmail.com>
2022-09-01 10:58:49 -0500

    Add Psuedo Code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
+// Psuedo Code:
+//Create an array using .from(arguments)
+//Test for non-number parameters using TypeOf
+//Test for nonpositive integers
+//Sort the elements from least to greatest
+// Define the elements as the bounds for a range and loop through the range using for...of
+//Sum all numbers 
+//Return the sumOfAllNumbers
+
 const sumAll = function() {
+    
 
 };
 
