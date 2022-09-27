c01f7a5b1639a885fdb971f6769f5f4a22445c32
solomonjamesa <myworkemail998800@gmail.com>
2022-09-27 13:50:37 -0500

    Create a function that converts a given argument value from a farenheit to celcius value and vice versa.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
 const ftoc = function() {
+  let F = arguments[0];
+  let celcius = ((F)-32)*(5/9);
+  let celciusRounded = celcius.toFixed(1);
+  let ftoc = Number(celciusRounded);
+  return ftoc;
 
 };
 
 const ctof = function() {
-
+  let C = arguments[0];
+  let farenheit = ((C)*(9/5)+32);
+  let farenheitRounded = farenheit.toFixed(1);
+  let ctof = Math.abs(Number(farenheitRounded));
+  return ctof;
+  
 };
 
 // Do not edit below this line
